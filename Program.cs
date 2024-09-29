5416cf7cdb5d0a06102e231ada34e450070bc3f3
Vlad Rusu <rusu.v2k@gmail.com>
2024-09-30 01:25:01 +0300

    Add entry point

---
+++ Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace UniversityManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu menu = new Menu();
+            menu.DisplayMenu();
+        }
+    }
+}
